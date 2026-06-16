9a8eeb1f2df9a9f5197415f759fdd61e054965fe
z94129902 <z94129902@gmail.com>
2026-06-16 17:27:26 +0800

    Create BankAccount.java

---
+++ week1/BankAccount.java
@@ -0,0 +1,8 @@
+Public class BankAccount {
+  private string Owner;
+  private int Balance;
+
+  Public BankAccount (string Owner, int Balance) {
+    this.Owner = Owner;
+    this balance = Balance;
+}
