1a03e1c34cccfee086c69037b43f645f23a5f5ea
z94129902 <z94129902@gmail.com>
2026-06-17 16:01:17 +0800

    Create Xunhuan.java

---
+++ week1/Xunhuan.java
@@ -0,0 +1,9 @@
+public class Xunhuan {
+        public static void main(String[] args) {
+                int x = 0;
+                while (x <= 10) {
+                        System.out.print("value of x : " + x );
+                        x++;
+                }
+        }
+}
