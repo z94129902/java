22fa9f3f16d2f3ff6bde00aaf515f4bc87dcb485
z94129902 <z94129902@gmail.com>
2026-06-22 16:22:15 +0800

    Create javastring.java

---
+++ week1/javastring.java
@@ -0,0 +1,7 @@
+public class javastring {
+        public static void main(String args[]) {
+                char [] wordarray = {'h', 'e', 'l', 'l', 'o'};
+                String word = new String(wordarray);
+                System.out.println( word );
+        }
+}
